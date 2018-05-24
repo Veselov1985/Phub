27cf0adbc44d658c049a41597469d2b25efb6cf3
Yurii Veselov <xoxorrrr@gmail.com>
2018-05-24 17:22:13 +0300

    new grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,OnInit ,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+ @ViewChild('vr') vr:ElementRef;
+  elem:any;
+  eframe:any;
+ 
+
+  constructor(ref:ElementRef){
+   this.elem=ref.nativeElement;
+  }
+  ngOnInit(){
+   this.eframe=this.elem.querySelector('a-scene');
+  }
+ 
 }
